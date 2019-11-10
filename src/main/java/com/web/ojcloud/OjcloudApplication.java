e54e4cdd4effee8416bea13aa0351e25ca165b28
wen <18813299794@163.com>
2019-11-10 17:27:53 +0800

    no message

---
+++ src/main/java/com/web/ojcloud/OjcloudApplication.java
@@ -0,0 +1,13 @@
+package com.web.ojcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjcloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OjcloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/web/ojcloud/OjcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.ojcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjcloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
